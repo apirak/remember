0692a3ca0d064f6a55b9de5b9764fcbcef1e8d85
Apirak Panatkool <apirakb@gmail.com>
2025-09-26 08:12:12 +0700

    refactor: Comment out DebugPanel in FlashcardAppContent for cleaner UI during development

--- src/components/flashcard/FlashcardApp.tsx
+++ src/components/flashcard/FlashcardApp.tsx
@@ -113,7 +113,7 @@ const FlashcardAppContent: React.FC = () => {
     <div className="min-h-screen bg-gradient-to-br from-blue-50 via-yellow-50 to-green-50">
       {renderCurrentScreen()}
       {/* Global Debug Panel - visible on all pages in development */}
-      <DebugPanel position="right" defaultVisible={false} />
+      {/* <DebugPanel position="right" defaultVisible={false} /> */}
     </div>
   );
 };
